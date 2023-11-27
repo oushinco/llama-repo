a236f7ea7ab624b8be83c6cff00ed53717b8299c
Xinguang Wang <wang1xin@police.nsw.gov.au>
2023-11-27 16:11:28 +1100

    docker2

--- start_service.sh
+++ start_service.sh
@@ -2,21 +2,23 @@
 
 # Start the controller
 # python -m llava.serve.controller --host 0.0.0.0 --port 10000 &
-python -m fastchat.serve.controller --host 0.0.0.0 --port 10000 &
+python -m fastchat.serve.controller --host 0.0.0.0 --port 21001 &
 
 # Sleep for a moment to allow the controller to start
 sleep 5
 
 # Start the Gradio web server
 #python -m llava.serve.gradio_web_server --controller http://localhost:10000 --model-list-mode reload --port 7860 &
-python -m fastchat.serve.gradio_web_server --controller http://localhost:10000 --model-list-mode reload --port 7860 & 
+python -m fastchat.serve.gradio_web_server --controller http://localhost:21001 --model-list-mode reload --port 7860 & 
 
 # Sleep for a moment to allow the controller to start
 sleep 15
 
 # Start the model worker
 # python -m llava.serve.model_worker --host 0.0.0.0 --controller http://localhost:10000 --port 40000 --worker http://localhost:40000 --model-path liuhaotian/llava-v1.5-7b --load-8bit &
-python -m fastchat.serve.model_worker --host 0.0.0.0 --controller http://localhost:10000 --model-path lmsys/vicuna-7b-v1.5 --device cpu & 
+# CUDA_VISIBLE_DEVICES=0 python -m fastchat.serve.model_worker --model-path lmsys/vicuna-7b-v1.5 --controller http://localhost:21001 --port 31000 --worker http://localhost:31000
+python -m fastchat.serve.model_worker --host 0.0.0.0 --controller http://localhost:21001 --port 31000 --worker http://localhost:31000 --model-path lmsys/vicuna-7b-v1.5 & 
+python -m fastchat.serve.test_message --model-name vicuna-7b-v1.5 &
 
 sleep 300
 
